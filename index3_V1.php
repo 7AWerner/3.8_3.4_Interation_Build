1411cc71e116f19556f1187ac57b5a7c540efa36
7AWerner <86210403+7AWerner@users.noreply.github.com>
2021-09-15 11:28:04 +1200

    Update index3_V1.php

--- index3_V1.php
+++ index3_V1.php
@@ -1,10 +1,11 @@
 <?php
+
 	ob_start();
 	session_start();
 		$error = NULL;
 		if($_SERVER["REQUEST_METHOD"] == "POST") {
 			//connect.php (tells where to connect servername, dbaseName, username, password)
-			require "../connect.php";
+			require "connect.php";
 			//username and password sent from form
 			$myusername = mysqli_real_escape_string($conn,$_POST['username']);
 			$mypassword = mysqli_real_escape_string($conn,$_POST['password']);
@@ -17,10 +18,23 @@
 			$count = mysqli_num_rows($result);
 			
 			// if result matched $myusername and $mypassword, table row must be 1 row
-			if($count == 1) {
-				$_SESSION['login_user'] = $myusername;
-				header("location: index.php");
-			} else { 
+			
+			if($count == 1 && $_SESSION['index3.php'] = $myusername) {
+
+				if($myusername == "Graeme") {
+					$_SESSION['login_user'] = $myusername;
+					header("location: admin.php");
+				}
+
+				else {
+					$_SESSION['login_user'] = $myusername;
+					header("location: index.php");
+				}
+			}
+
+			// if result matched $myusername and $mypassword, table row must be 1 row
+			
+			else { 
 				$error = "ERROR! Your Login Name or Password is invalid";
 				}
 			}
@@ -31,28 +45,23 @@
 <link rel="stylesheet" href="css/style2.css"/>
 </head>
 <body>
-	<header class="page-header">
-		<div class = "header">				
-		<h1 style="font-size:3vw;">ROK Music</h1>	
-
-		</div>
-	</header>
 		<div class = "container">
 			<div class = "nav">
-				<img src="../images/banner2.jpg">
-				<a  class="btn" href="index3.php"><h2 style="font-size:2vw;">Sign Up Today</h2></a>
+				<img src="images/banner.jpg">
+				<div style="font-size:3vw;" class="middle"><h1>ROK Music</h1></div>
   			</div>				
 	
 			<div class="section"><!-- Holds the main page content -->
-				<h1 style="font-size:2vw;">Sign In</h1>
+				<h2 style="font-size:2vw;">Sign In</h2>
 				<hr>
-					<h1><form method = "post" id = "index3.php">
+					<h2><form method = "post" id = "index3.php">
 						<label for = 'login' style="font-size:2vw;">Username:</label>
 						<input style="font-size:1vw;" type = "text" name  = "username" placeholder = "Enter username"/><br/>
 						<label for = 'login' style="font-size:2vw;">Password:</label>
 						<input style="font-size:1vw;" type = "password" name = "password" placeholder = "Enter password"/><br/>
-						<button type = "submit" style="float: right;"><h2 style="font-size:1.5vw;">Login In</h2></button>
-					</form></h1>
+						<button type = "submit" style="float: right;"><h3 style="font-size:1.5vw;">Login In</h3></button>
+						<a href="user/add.php" style="float: right;"><h3 style="font-size:1.5vw;">Sign Up</h3></a>
+					</form></h2>
 					<h3 class = "grey"><?php echo $error; ?></h3>
 			</div>					
 	</div>
